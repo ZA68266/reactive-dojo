5c86027f6dc5dc0e87e3dad55be34129499483bb
ZA68266 <zachary.anderson2@kroger.com>
2021-08-18 15:17:58 -0400

    first commit

---
+++ src/main/java/com/example/reactivedojo/ReactiveDojoApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactivedojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveDojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveDojoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reactivedojo/ReactiveDojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactivedojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveDojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
